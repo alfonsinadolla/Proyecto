b7bde54b868a06df5d3b88da53a6dd6184ed066d
alfonsinadolla <128007627+alfonsinadolla@users.noreply.github.com>
2023-06-12 23:53:12 -0300

    Add files via upload

--- etiquetar_imagenes.py
+++ etiquetar_imagenes.py
@@ -11,39 +11,43 @@ import inicio_ventana
 class ventanaEtiquetar:
 
     def __init__(self):
-         """
-           Inicializa la clase ventanaEtiquetar.
-
-           Crea las columnas de la interfaz gráfica.
         """
-         
-         left_col = [[sg.Text('Bienvenido a Etiquetar Imagenes',font=('Arial', 12), background_color='white', text_color='black')],
-                [sg.Text('Folder',background_color='white', text_color='black'), sg.In(size=(25,1),background_color='white', text_color='black', enable_events=True ,key='-FOLDER-'), sg.FolderBrowse(button_color='black')],
-                [sg.Listbox(values=[], enable_events=True, size=(40,20),key='-FILE LIST-', background_color='white', text_color= 'black')],
-                [sg.Text('Tags',background_color='white', text_color='black')],
-                [sg.Input(key='-TAGS-',background_color='white', text_color='black'), sg.Button('Agregar',button_color='black',key='AgregarTags')],
-                [sg.Text('Descripción',background_color='white', text_color='black')],
-                [sg.Input(key='-DESC-',background_color='white', text_color='black'), sg.Button('Agregar',button_color='black',key='AgregarDesc')]]
+        Inicializa la clase ventanaEtiquetar.
 
-         right_col = [[sg.Button('Volver',button_color='black',size=(10,2),pad=((280,0),(0,0)))],
-                        [sg.Text(size=(40,1),text_color='black',background_color='white', key='-TOUT-')],
-                        [sg.Image(key='-IMAGE-', size=(340,340))],
-                        [sg.Text('Tamaño:',background_color='white', text_color='black',key='-TAMAÑO-')],
-                        [sg.Text('Tipo:',background_color='white', text_color='black',key='-TIPO-')],
-                        [sg.Text('Resolucion:',background_color='white', text_color='black',key=('-RESOLUCION-'))],
-                        [sg.Text("",background_color='white', text_color='black', key="-TAGVALUE-")],
-                        [sg.Text("",background_color='white', text_color='black', key="-DESCVALUE-")],
-                        [sg.Push(background_color='white'),sg.Button('Guardar',button_color='black',key='-GUARDAR-',size=(10,2))]]
+        Crea las columnas de la interfaz gráfica.
+        """
+        
+        left_col = [
+            [sg.Text('Bienvenido a Etiquetar Imagenes', font=('Arial', 12))],
+            [sg.Text('Folder'), sg.In(size=(25, 1), background_color='white', enable_events=True, key='-FOLDER-'), sg.FolderBrowse()],
+            [sg.Listbox(values=[], enable_events=True, size=(40, 20), key='-FILE LIST-')],
+            [sg.Text('Tags')],
+            [sg.Input(key='-TAGS-', background_color='white'),
+             sg.Button('Agregar', key='AgregarTags')],
+            [sg.Text('Descripción')],
+            [sg.Input(key='-DESC-', background_color='white'),
+             sg.Button('Agregar', key='AgregarDesc')]
+        ]
 
+        right_col = [
+            [sg.Button('Volver', size=(10, 2), pad=((280, 0), (0, 0)))],
+            [sg.Text(size=(40, 1), background_color='white', key='-TOUT-')],
+            [sg.Image(background_color='white', key='-IMAGE-', size=(340, 340))],
+            [sg.Text('Tamaño:', background_color='white', key='-TAMAÑO-')],
+            [sg.Text('Tipo:', background_color='white', key='-TIPO-')],
+            [sg.Text('Resolucion:', background_color='white', key=('-RESOLUCION-'))],
+            [sg.Text("", background_color='white', key="-TAGVALUE-")],
+            [sg.Text("", background_color='white', key="-DESCVALUE-")],
+            [sg.Push(background_color='white'), sg.Button('Guardar', key='-GUARDAR-', size=(10, 2))]
+        ]
 
-         layout = [[sg.Column(left_col,background_color='white'), sg.Column(right_col,background_color='white')]]
+        layout = [[sg.Column(left_col, background_color='white'), sg.Column(right_col, background_color='white')]]
 
-         #Crea la ventana con el layout
-         self.window = sg.Window('Etiquetar Imágenes', layout,size=(800,600),background_color='white')
-         
-    def convert_to_bytes(self,file_or_bytes, resize=None):
-         
-         """
+        # Crea la ventana con el layout
+        self.window = sg.Window('UNLPImage', layout, size=(800, 600), background_color='white')
+
+    def convert_to_bytes(self, file_or_bytes, resize=None):
+        """
         Convierte una imagen en bytes y, opcionalmente, cambia su tamaño.
 
         Args:
@@ -52,24 +56,24 @@ class ventanaEtiquetar:
 
         Devuelve la imagen convertida en bytes.
         """
-         if isinstance(file_or_bytes, str):
+        if isinstance(file_or_bytes, str):
             img = PIL.Image.open(file_or_bytes)
-         else:
+        else:
             try:
                 img = PIL.Image.open(io.BytesIO(base64.b64decode(file_or_bytes)))
             except Exception as e:
                 dataBytesIO = io.BytesIO(file_or_bytes)
                 img = PIL.Image.open(dataBytesIO)
 
-         cur_width, cur_height = img.size
-         if resize:
+        cur_width, cur_height = img.size
+        if resize:
             new_width, new_height = resize
-            scale = min(new_height/cur_height, new_width/cur_width)
-            img = img.resize((int(cur_width*scale), int(cur_height*scale)), PIL.Image.ANTIALIAS)
-         bio = io.BytesIO()
-         img.save(bio, format="PNG")
-         del img
-         return bio.getvalue()
+            scale = min(new_height / cur_height, new_width / cur_width)
+            img = img.resize((int(cur_width * scale), int(cur_height * scale)), PIL.Image.ANTIALIAS)
+        bio = io.BytesIO()
+        img.save(bio, format="PNG")
+        del img
+        return bio.getvalue()
 
     def imagen_existe(self,ruta_imagen, ruta_csv):
         """
@@ -148,7 +152,6 @@ class ventanaEtiquetar:
             if event == sg.WINDOW_CLOSED:
                 break
             if event == 'AgregarTags':
-                '''Obtenemos los valores de los campos'''
                 tags = values['-TAGS-']
                 self.window.Element("-TAGVALUE-").Update(tags)
             if event == 'AgregarDesc':
@@ -193,7 +196,7 @@ class ventanaEtiquetar:
                     with open('logs.csv', 'a', encoding ='UTF-8') as logs:
                         c = csv.writer(logs)
                         c.writerow([perfil_act,datetime.datetime.now(),'modifico una nueva imagen clasificada'])
-                sg.popup_no_border('Se guardo correctamente la metadata de la imagen')
+                sg.popup_no_border('Se guardo correctamente la metadata de la imagen', button_color='black', background_color='white', text_color='black', )
             elif event == 'volver' or event == sg.WIN_CLOSED:
                 menu = menu_principal_ventana.VentanaMenu()
                 menu.iniciar_ventana()
